5419e6e9f167bc9d508fff8359ae22227b23ecc7
x-cold <lxstart@outlook.com>
2015-10-07 17:18:01 +0800

    bak

--- public/js/config.js
+++ public/js/config.js
@@ -6,5 +6,7 @@ require.config({
 
 require(['jquery'], function($) {
 	$('#header').load('./_partial/header.html');
+	$('#wrapper').load('./_partial/task-list.html');
+	$('#aside').load('./_partial/aside.html');
 	$('#footer').load('./_partial/footer.html');
 });
